b20e4a5852be350a15af2560ab3f8d8c70402350
Oktavium <oktaviumir@yandex.ru>
2023-09-29 15:27:54 +0300

    Решил задача 54

--- Задача_54/Program.cs
+++ Задача_54/Program.cs
@@ -4,5 +4,65 @@
 // 5 9 2 3                         9 5 3 2
 // 8 4 2 4                         8 4 4 2
 
+//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Clear();
+int SetNumber(string message)
+{
+    Console.Write($"Введите колличество {message}: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int[,] GetMatrix(int rows, int columns, int minValue = 10, int maxValue = 99)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+            matrix[i, l] = rand.Next(minValue, maxValue + 1);
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
 
 
+int rows = SetNumber("rows");
+int columns = SetNumber("columns");
+Console.WriteLine("\nИсходный массив: ");
+int[,] matrix = GetMatrix(rows, columns);
+PrintMatrix(matrix);
+Console.WriteLine("\nОтсортированный массив ");
+SortMatrix(matrix);
+PrintMatrix(matrix);
